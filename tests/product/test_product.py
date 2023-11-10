56226edfcc86ebc1000cae189f55349441d1b189
Vinícius Souza <tradutorvinicius@gmail.com>
2023-11-10 08:47:09 -0300

    Resolve requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        '1',
+        'NoProduct',
+        'NoName',
+        '01/11/2023',
+        '30/11/2024',
+        '12345',
+        'NoInstructions',
+    )
+
+    assert product.id == '1'
+    assert product.company_name == 'NoName'
+    assert product.product_name == 'NoProduct'
+    assert product.manufacturing_date == '01/11/2023'
+    assert product.expiration_date == '30/11/2024'
+    assert product.serial_number == '12345'
+    assert product.storage_instructions == 'NoInstructions'
