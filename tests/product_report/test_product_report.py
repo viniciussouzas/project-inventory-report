7b4a247ebc0cb2394aec2a00317884b82e04daa7
Vinícius Souza <tradutorvinicius@gmail.com>
2023-11-10 08:51:27 -0300

    Resolve requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        '1',
+        'NoProduct',
+        'NoName',
+        '01/11/2023',
+        '30/11/2024',
+        '12345',
+        'NoInstructions',
+    )
+
+    assert str(product) == (
+        "The product 1 - NoProduct "
+        "with serial number 12345 "
+        "manufactured on 01/11/2023 "
+        "by the company NoName "
+        "valid until 30/11/2024 "
+        "must be stored according to the following instructions: "
+        "NoInstructions."
+    )
