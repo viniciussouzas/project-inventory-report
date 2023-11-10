9190788a7c74babec03294ac9ea63f69943fc78c
Vinícius Souza <tradutorvinicius@gmail.com>
2023-11-10 09:00:14 -0300

    Resolve requisito 3

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,8 +1,15 @@
 from typing import Dict, Type
+from abc import ABC, abstractmethod
+from inventory_report.product import Product
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> list[Product]:
+        ...
 
 
 class JsonImporter:
